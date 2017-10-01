6efbbe022301226d4b841e5c755a9315843df00c
zm1try <zmitryarosh@gmail.com>
2017-10-01 20:26:18 +0300

    23/25. filter is hard.

--- src/fsm.js
+++ src/fsm.js
@@ -5,10 +5,12 @@ class FSM {
      */
     
     constructor(config) {
-        if (!config)
-            throw new Error();
-        else this.info = config;
-
+        if (typeof config == 'undefined')
+            throw new Error('config isn\'t passed');
+        this.config = config;
+        this.stack = [config['initial']];
+        this.index = 0;
+        this.available = 0;
     }
     
         
@@ -17,7 +19,7 @@ class FSM {
      * @returns {String}
      */
     getState() {
-        return this.info.initial;
+        return this.stack[this.index];        
     }
 
     /**
@@ -25,77 +27,11 @@ class FSM {
      * @param state
      */
     changeState(state) {
-
-        if (state === 'normal' || state === 'busy' || state === 'sleeping'|| state === 'hungry') var a = 1;
-        else throw new Error();
-
-        
-        if (this.info.initial === state) {
-            //console.log(this.info.initial);
-            return this.info.initial;}
-        else {
-            switch(this.info.initial) {
-                case 'normal': 
-                    switch(state) {
-                        case 'busy': 
-                            this.info.initial = this.info.states.normal.transitions.study;
-                            break;
-                        case 'sleeping':
-                            this.info.initial = this.info.states.normal.transitions.study;
-                            this.info.initial = this.info.states.busy.transitions.get_tired;
-                            break;
-                        case 'hungry':
-                            this.info.initial = this.info.states.normal.transitions.study;
-                            this.info.initial = this.info.states.busy.transitions.get_hungry;
-                            break;
-                    };
-                    break;
-                case 'busy':
-                    switch(state) {
-                        case 'normal':
-                            this.info.initial = this.info.states.busy.transitions.get_hungry;
-                            this.info.initial = this.info.states.hungry.transitions.eat;
-                            break;
-                        case 'sleeping':
-                            this.info.initial = this.info.states.busy.transitions.get_tired;
-                            break;
-                        case 'hungry':
-                            this.info.initial = this.info.states.busy.transitions.get_hungry;
-                            break;
-                    };
-                    break;
-                case 'sleeping':
-                    switch(state) {
-                        case 'normal':
-                            this.info.initial = this.info.states.sleeping.transitions.get_up;
-                            break;
-                        case 'busy':
-                            this.info.initial = this.info.states.sleeping.transitions.get_up;
-                            this.info.initial = this.info.states.normal.transitions.study;
-                            break;
-                        case 'hungry':
-                            this.info.initial = this.info.states.sleeping.transitions.get_hungry;
-                            break;
-                    };
-                    break;
-                case 'hungry':
-                    switch(state) {
-                        case 'normal':
-                            this.info.initial = this.info.states.hungry.transitions.eat;
-                            break;
-                        case 'busy':
-                            this.info.initial = this.info.states.hungry.transitions.eat;
-                            this.info.initial = this.info.states.normal.transitions.study;
-                            break;
-                        case 'sleeping':
-                            this.info.initial = this.info.states.hungry.transitions.eat;
-                            this.info.initial = this.info.states.normal.transitions.study;
-                            this.info.initial = this.info.states.busy.transitions.get_tired;
-                            break;
-                    };
-                    break;
-            }
-        }   return this.info.initial;
+        if (!(state in this.config['states']))
+            throw new Error('state isn\'t exist');
+        this.stack.push(state);
+        this.index++;
+        this.available = 0;
     }
 
     /**
@@ -103,146 +39,19 @@ class FSM {
      * @param event
      */
     trigger(event) {
-        
-        /*switch(this.info.initial) {
-            case 'normal': 
-                if (event == 'study') break;
-                else throw new Error();
-            case 'busy':
-                if (event == 'get_tired'|| event === 'get_hungry') break;
-                else throw new Error();
-            case 'sleeping':
-                if (event == 'get_hungry'|| event === 'get_up') break;
-                else throw new Error();
-            case 'hungry':
-                if (event == 'eat') break;
-                else throw new Error();
-        }*/
-
-        /*if (event === 'study' || event === 'eat' || event === 'get_tired'|| event === 'get_hungry'|| event === 'get_up') var a = 1;
-        else throw new Error;
-        console.log(this.info.initial);*/
-
-        switch(this.info.initial) {
-            case 'normal':
-                switch(event) {
-                    case 'study':
-                        this.info.initial = this.info.states.normal.transitions.study;
-                        break;
-                    case 'eat':
-                        /*this.info.initial = this.info.states.normal.transitions.study;
-                        this.info.initial = this.info.states.busy.transitions.get_hungry;
-                        this.info.initial = this.info.states.hungry.transitions.eat;*/
-                        break;
-                    case 'get_tired':
-                        this.info.initial = this.info.states.normal.transitions.study;
-                        this.info.initial = this.info.states.busy.transitions.get_tired;
-                        break;
-                    case 'get_hungry':
-                        this.info.initial = this.info.states.normal.transitions.study;
-                        this.info.initial = this.info.states.busy.transitions.get_hungry;
-                        break;
-                    case 'get_up':
-                        /*this.info.initial = this.info.states.normal.transitions.study;
-                        this.info.initial = this.info.states.busy.transitions.get_hungry;*/
-                        break;
-                    /*default: throw new Error;
-                        break;*/
-                };
-                break;
-            case 'busy':
-                switch(event) {
-                    case 'study':
-                        break;
-                    case 'eat':
-                        this.info.initial = this.info.states.busy.transitions.get_hungry;
-                        this.info.initial = this.info.states.hungry.transitions.eat;
-                        break;
-                    case 'get_tired':
-                        this.info.initial = this.info.states.busy.transitions.get_tired;
-                        break;
-                    case 'get_hungry':
-                        this.info.initial = this.info.states.busy.transitions.get_hungry;
-                        break;
-                    case 'get_up':
-                        this.info.initial = this.info.states.busy.transitions.get_tired;
-                        this.info.initial = this.info.states.sleeping.transitions.get_up;
-                        break;
-                    /*default: throw new Error;
-                        break;*/
-                };
-                break;
-            case 'sleeping':
-                switch(event) {
-                    case 'study':
-                        this.info.initial = this.info.states.sleeping.transitions.get_up;
-                        this.info.initial = this.info.states.normal.transitions.study;
-                        break;
-                    case 'eat':
-                        this.info.initial = this.info.states.sleeping.transitions.get_hungry;
-                        this.info.initial = this.info.states.hungry.transitions.eat;
-                        break;
-                    case 'get_tired':
-                        break;
-                    case 'get_hungry':
-                        this.info.initial = this.info.states.sleeping.transitions.get_hungry;
-                        break;
-                    case 'get_up':
-                        this.info.initial = this.info.states.sleeping.transitions.get_up;
-                        break;
-                    /*default: throw new Error;
-                        break;*/
-                };
-                break;
-            case 'hungry':
-                switch(event) {
-                    case 'study':
-                        this.info.initial = this.info.states.hungry.transitions.eat;
-                        this.info.initial = this.info.states.normal.transitions.study;
-                        break;
-                    case 'eat':
-                        this.info.initial = this.info.states.hungry.transitions.eat;
-                        break;
-                    case 'get_tired':
-                        this.info.initial = this.info.states.normal.transitions.study;
-                        this.info.initial = this.info.states.busy.transitions.get_tired;
-                        break;
-                    case 'get_hungry':
-                        break;
-                    case 'get_up':
-                        this.info.initial = this.info.states.busy.transitions.get_hungry;
-                        this.info.initial = this.info.states.busy.transitions.get_tired;
-                        this.info.initial = this.info.states.sleeping.transitions.get_up;
-                        break;
-                    /*default: throw new Error;
-                        break;*/
-                };
-                break;
-        };
-        return this.info.initial;
-        
+        if (!(event in this.config.states[this.stack[this.index]].transitions))
+            throw new Error('event in current state isn\'t exist');
+        this.stack.push(this.config.states[this.stack[this.index]].transitions[event]);
+        this.index++;
+        this.available = 0;
     }
 
     /**
      * Resets FSM state to initial.
      */
     reset() {
-
-        switch(this.info.initial) {
-            case 'normal': 
-                break;
-            case 'busy':
-                this.info.initial = this.info.states.busy.transitions.get_hungry;
-                this.info.initial = this.info.states.hungry.transitions.eat;
-                break;
-            case 'sleeping':
-                this.info.initial = this.info.states.sleeping.transitions.get_up;
-                break;
-            case 'hungry':
-                this.info.initial = this.info.states.hungry.transitions.eat;
-                break;
-        }
-
+        this.stack.push(this.config['initial']);
+        this.index++;
     }
 
     /**
@@ -252,30 +61,7 @@ class FSM {
      * @returns {Array}
      */
     getStates(event) {
-
-        if (!event) return ['normal','busy','hungry','sleeping'];
-        switch(event) {
-            case 'study':
-                return ['normal'];
-                break;
-            case 'eat':
-                return ['hungry'];
-                break;
-            case 'get_tired':
-                return ['busy'];
-                break;
-            case 'get_hungry':
-                return ['busy','sleeping'];
-                break;
-            case 'get_up':
-                return ['sleeping'];
-                break;
-            default: 
-                return [];
-                break;
-        };
-        //console.log(this.info.states);
-        //if(!event) return this.info;
+        return Object.keys(this.config.states);
     }
 
     /**
@@ -284,24 +70,11 @@ class FSM {
      * @returns {Boolean}
      */
     undo() {
-        if(this.info.initial == 'normal') return false;
-        else {
-            switch(this.info.initial) {
-                case 'busy':
-                    this.info.initial = this.info.states.busy.transitions.get_hungry;
-                    this.info.initial = this.info.states.hungry.transitions.eat;
-                    return true;
-                    break;
-                case 'sleeping':
-                    this.info.initial = this.info.states.sleeping.transitions.get_up;
-                    return true;
-                    break;
-                case 'hungry':
-                    this.info.initial = this.info.states.hungry.transitions.eat;
-                    return true;
-                    break;
-            }
-        }
+        if (this.index == 0)
+            return false;
+        this.index--;
+        this.available++;
+        return true;
     }
 
     /**
@@ -310,13 +83,22 @@ class FSM {
      * @returns {Boolean}
      */
     redo() {
-        if(this.info.initial == 'normal') return false;
+        if (this.available == 0){
+            return false;
+        }
+        this.index++;
+        this.available--;
+        return true;
     }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.stack = [this.config['initial']];
+        this.index = 0;
+        this.available = 0;
+    }
 }
 
 module.exports = FSM;
